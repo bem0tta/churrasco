486408c98098ae3bfaef2a92ad272c5caae7e360
bem0tta <blzmotta@gmail.com>
2026-09-22 11:06:36 -0300

    editar e atualizar 1.0

---
+++ participantes/atualizar.php
@@ -0,0 +1,32 @@
+<?php
+    require_once '../includes/validar_login.php';
+    require_once '../config/conexao.php';
+
+    $id = $_GET['id'];
+    $nome = $_POST['nome'];
+    $turma = $_POST['turma'];
+    $telefone = $_POST['telefone'];
+    $tipo = $_POST['tipo'];
+    $acompanhamento = $_POST['acompanhamento'];
+    $presenca = $_POST['presenca'];
+    $pagamento = $_POST['pagamento'];
+
+    $sql = "UPDATE participantes
+        SET nome = '$nome',
+            turma = '$turma',
+            telefone = '$telefone',
+            tipo_churrasco = '$tipo',
+            acompanhamento = '$acompanhamento',
+            confirmado = '$presenca',
+            pago = '$pagamento'
+        WHERE id = '$id'";
+
+    try{
+        $con->query($sql);
+        Seaslog::alert('Participante cadastrado!');
+        header('location: listar.php');
+    } catch(mysqli_sql_exception $e){
+        Seaslog::alert('Não foi possível cadastrar!');
+        header('location: cadastrar.php');
+    }
+?>
---
+++ participantes/editar.php
@@ -0,0 +1,53 @@
+<?php
+    require_once '../includes/validar_login.php';
+    if(!isset($_GET['id'])){
+        header('location: index.php');
+    }
+    $sql = 'select * from participantes where id = '.$_GET['id'].';';
+    $res = $con->query($sql);
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar</title>
+</head>
+<body>
+    <form action="atualizar.php?id=<?php echo $_GET['id']?>" method="post">
+        <label>Nome 
+            <input type="text" name="nome" value = "<?php echo $res->nome?>">
+        </label>
+
+        <label>Turma 
+            <input type="text" name="turma" value = "<?php echo $res->turma?>">
+        </label>
+
+        <label>Telefone 
+            <input type="text" name="telefone" value = "<?php echo $res->telefone?>">
+        </label>
+
+        <label>Tipo de churrasco
+            <select name="tipo" value = "<?php echo $res->tipo_churrasco?>">
+                <option value="t" >Tradicional</option>
+                <option value="v">Vegetariano</option>
+            </select>
+        </label>
+
+        <label>Acompanhamento
+            <input type="text" name="acompanhamento" value = "<?php echo $res->acompanhamento?>">
+        </label>
+
+        <p>Presença confirmada</p>
+        <label><input type="radio" value="1" name="presenca" <?php $res->confirmado ? 'checked' : ''?>> Sim</label>
+        <label><input type="radio" value="0" name="presenca" <?php $res->confirmado ? '' : 'checked'?>> Não</label>
+
+        <p>Pagamento realizado</p>
+        <label><input type="radio" value= "1" name="pagamento" <?php $res->pago ? 'checked' : ''?>> Sim</label>
+        <label><input type="radio" value= "0" name="pagamento" <?php $res->pago ? '' : 'checked'?>> Não</label>
+
+        <button type="submit">ENVIAR</button>
+    </form>
+</body>
+</html>
