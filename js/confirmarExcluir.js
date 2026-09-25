4e826aeaf23be4ba9137e3bc95dadd999d5793a7
polettoluis <luissfer283@gmail.com>
2026-09-25 10:08:38 -0300

    arrumando foreach

--- js/confirmarExcluir.js
+++ js/confirmarExcluir.js
@@ -1,4 +1,4 @@
-document.getElementsByClassName('excluir').forEach(element => {
+document.querySelectorAll('.excluir').forEach(element => {
     element.addEventListener('click',()=>{
         if (element.innerText == "Confirmar?") {
             window.location.assign('excluir.php?id='+element.dataset.id)
